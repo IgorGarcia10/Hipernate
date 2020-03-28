4c09396f6de6b985af6038a7fcfb4583d2da45dd
Igor Garcia <alexandre.igor64@gmail.com>
2020-03-27 21:40:41 -0300

    Aula 03

---
+++ tempo_hibernate/src/main/java/br/usjt/tempo_hibernate/TempoHibernateApplication.java
@@ -0,0 +1,13 @@
+package br.usjt.tempo_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempoHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempoHibernateApplication.class, args);
+	}
+
+}
---
+++ tempo_hibernate/src/test/java/br/usjt/tempo_hibernate/TempoHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usjt.tempo_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempoHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
